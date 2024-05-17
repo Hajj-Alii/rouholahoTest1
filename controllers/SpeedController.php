b06609431da796aa74b9ab68a6789bd874f23b66
amin firouzi bandpey <aminfirouzibandpey@gmail.com>
2024-05-17 20:09:17 +0330

    DateTime converted into jalali

--- controllers/SpeedController.php
+++ controllers/SpeedController.php
@@ -7,10 +7,17 @@ class SpeedController
 
     public static $speedCondition;
 
-    public static function valueExist()
+    public static function valueExist($paramName)
     {
-        if (isset($_GET["spd"]) && !empty($_GET["spd"]))
+        if (isset($_GET["$paramName"]) && !empty($_GET[$paramName]))
             return true;
+        return false;
+    }
+
+    public static function valuesExist($paramNames = array()){
+        foreach ($paramNames as $name)
+            if(self::valueExist($name))
+                return true;
         return false;
     }
 
@@ -22,6 +29,33 @@ class SpeedController
         else
             echo "parameter Undefined!";
     }
+
+
+    public static function add5thSpeed($key, $value, $time)
+    {
+        if(self::valueExist($key))
+        {
+            self::$speed = new SpeedModel();
+
+            self::$speed::insertSpd($value, $time);
+
+        }
+        else
+            echo "Undefined parameter $value";
+    }
+
+//    public static function addSpeeds_Items($valueArray = array() ,$time){
+//
+//        if(self::valuesExist($valueArray))
+//            {
+//                self::$speed = new SpeedModel();
+//                $interval = new DateInterval('PT5M');
+//                $time->sub($interval);
+//                self::$speed::insertSpd($valueArray[0], $time);
+//
+//
+//            }
+//}
 
     public static function readAll(){
 
--- views/speedProcess.php
+++ views/speedProcess.php
@@ -1,11 +1,65 @@
  <?php
 include_once $_SERVER["DOCUMENT_ROOT"]. "/www/rouholahoTest1/controllers/SpeedController.php";
-
+include  $_SERVER["DOCUMENT_ROOT"]. "/www/rouholahoTest1/"."vendor/autoload.php";
+use \Morilog\Jalali\Jalalian;
 $ctrl = new SpeedController();
-$currentDateTime = new DateTime();
-$date = $currentDateTime->format('Y-m-d h-m-s');
-var_dump($date);
+// $timezone = new DateTimeZone('Asia/Tehran');
 
-//var_dump((int)$_GET["spd"]);
-$ctrl::addSpeed((int)$_GET["spd"],  $currentDateTime);
-//$ctrl::readAll();
+//
+// $date = \Morilog\Jalali\Jalalian::forge('now')->getTimestamp(); // 1333857600
+//echo jdate($date);
+// $date = new jDateTime(true, true, 'Asia/Tehran');
+//
+// echo $date->date("Y-m-d h:i:s", false, false)."\n";
+
+$timeZone = new DateTimeZone("Asia/Tehran");
+$newDate = Jalalian::forge('now', $timeZone);
+echo $newDate;
+//$ctrl::add5thSpeed('spd',$_GET['spd'], $newDate);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //// $jDate = \Morilog\Jalali\Jalalian::now();
+//// $date = \Morilog\Jalali\Jalalian::forge('now'); // 1391-10-02 00:00:00
+// $timestamp = \Morilog\Jalali\Jalalian::forge('last sunday');
+//$jDate = jdate(\Morilog\Jalali\Jalalian::forge('now'));
+//
+// //echo $date;
+//// $date =\Morilog\Jalali\Jalalian::forge('last sunday')->format('time'); // 00:00:00
+//echo $timestamp;
+
+// $DateTime = new DateTime("2023-07-25");
+
+// $IntlDateFormatter = new IntlDateFormatter(
+//     'ir_IR@calendar=persian',
+//     IntlDateFormatter::NONE,
+//     IntlDateFormatter::NONE,
+//     'Asia/Tehran',
+//     IntlDateFormatter::TRADITIONAL,
+//     "Y-m-d H:i:s"
+// );
+
+// echo $IntlDateFormatter->format($date);
+
+
+//$ctrl::addSpeed((int)$_GET["spd"],  $date);
