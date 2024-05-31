8d5df1600f54930a8ef67f5b21ed3fc0faeec8bc
amin firouzi bandpey <aminfirouzibandpey@gmail.com>
2024-05-31 22:19:31 +0330

    testTiming.php

--- views/testTiming.php
+++ views/testTiming.php
@@ -3,10 +3,10 @@ include_once $_SERVER["DOCUMENT_ROOT"] . "/www/rouholahoTest1/models/SpeedModel.
 require $_SERVER["DOCUMENT_ROOT"] . "/www/rouholahoTest1/controllers/SpeedController.php";
 
 
-$ctrl = new SpeedController();
-$records = $ctrl::fetchRecords_jalaliToGregorian("۱۴۰۳/۰۳/۰۲ ۱۳:۲۴:۴۸", "۱۴۰۳/۰۳/۰۲ ۱۶:۲۴:۴۸");
-var_dump(SpeedModel::getRawRecords(new DateTime("2024-05-22 14:12:14"), new DateTime("2024-05-24 09:26:27")));
-$formattedRecords = [];
+//$ctrl = new SpeedController();
+//$records = $ctrl::fetchRecords_jalaliToGregorian("۱۴۰۳/۰۳/۰۲ ۱۳:۲۴:۴۸", "۱۴۰۳/۰۳/۰۲ ۱۶:۲۴:۴۸");
+//var_dump(SpeedModel::getRawRecords(new DateTime("2024-05-22 14:12:14"), new DateTime("2024-05-24 09:26:27")));
+//$formattedRecords = [];
 //
 //foreach ($records as $record) {
 //    // Determine the value based on the status
@@ -31,4 +31,11 @@ $formattedRecords = [];
 
 // Output the JSON response
 //var_dump($records);
+
+
+
+//var_dump(SpeedController::readAll());
+
+var_dump(SpeedModel::getRecords(new DateTime("2024-05-31 19:22:51", new DateTimeZone("Asia/Tehran")), new DateTime("2024-05-31 19:25:51", new DateTimeZone("Asia/Tehran"))));
+
 ?>
