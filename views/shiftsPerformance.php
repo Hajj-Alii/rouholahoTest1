946c6d0dd856af9f0c200f773866e06bad5c72fc
amin firouzi bandpey <aminfirouzibandpey@gmail.com>
2024-06-02 10:10:08 +0330

    shiftsPerformance.php View updated

--- views/shiftsPerformance.php
+++ views/shiftsPerformance.php
@@ -1,32 +1,37 @@
-<div class="container">
+<div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
-            <form id="shiftPerformanceForm" class="form-inline">
-                <div class="form-group mb-2">
-                    <label for="startDate">تاریخ شروع:</label>
-                    <input type="text" class="form-control form-control mx-2" id="startDate" name="startDate" placeholder="تاریخ شروع">
+        <div class="col-12">
+            <form id="shiftPerformanceForm">
+                <div class="form-row">
+                    <div class="form-group col-md-4 mb-2">
+                        <label for="startDate">تاریخ شروع:</label>
+                        <input type="text" class="form-control" id="startDate" name="startDate" placeholder="تاریخ شروع">
+                    </div>
+                    <div class="form-group col-md-4 mb-2">
+                        <label for="endDate">تاریخ پایان:</label>
+                        <input type="text" class="form-control" id="endDate" name="endDate" placeholder="تاریخ پایان">
+                    </div>
+                    <div class="form-group col-md-4 mb-2">
+                        <label for="shift">شیفت:</label>
+                        <select class="form-control" id="shift" name="shift">
+                            <option value="A">A</option>
+                            <option value="B">B</option>
+                            <option value="C">C</option>
+                        </select>
+                    </div>
                 </div>
-                <div class="form-group mb-2">
-                    <label for="endDate" class="">تاریخ پایان:</label>
-                    <input type="text" class="form-control form-control mx-2" id="endDate" name="endDate" placeholder="تاریخ پایان">
+                <div class="form-row">
+                    <div class="form-group col-md-12">
+                        <button type="submit" class="btn btn-primary btn-primary">نمایش</button>
+                    </div>
                 </div>
-                <div class="form-group mb-2">
-                    <label for="shift" class="">شیفت:</label>
-                    <select class="form-control form-control mx-2" id="shift" name="shift">
-                        <option value="A">A</option>
-                        <option value="B">B</option>
-                        <option value="C">C</option>
-                    </select>
-                </div>
-                <button type="submit" class="btn btn-primary btn-sm mb-2">نمایش</button>
             </form>
-
         </div>
-
-
-
     </div>
-
-    <div id="shiftPerformanceResult"></div>
-    <p id="shiftPerformanceCalculations"></p>
+    <div class="row">
+        <div class="col-12">
+            <div id="shiftPerformanceResult"></div>
+            <p id="shiftPerformanceCalculations"></p>
+        </div>
+    </div>
 </div>
