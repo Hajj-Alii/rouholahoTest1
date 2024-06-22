c1a8d10f606ebaef253cbcde0d708c9696d8461f
amin firouzi bandpey <aminfirouzibandpey@gmail.com>
2024-06-22 14:20:39 +0330

    productionTonnage and shiftPerf bugs fixed

--- views/updateSpeed.php
+++ views/updateSpeed.php
@@ -1,18 +1,18 @@
 <?php
 require $_SERVER["DOCUMENT_ROOT"] . "/www/rouholahoTest1/controllers/ParamsController.php";
 
-header('Content-Type: application/json');
-
-
-
-try {
-    $result = ParamsController::fetchParams2("۱۴۰۳/۰۳/۱۲ ۱۳:۰۱:۳۷", "۱۴۰۳/۰۳/۲۸ ۱۳:۰۱:۳۷");
-
-    if (empty($result['params'])) {
-        echo json_encode(['params' => [], 'hasSpeedRecords' => $result['hasSpeedRecords']]);
-    } else {
-        echo json_encode($result);
-    }
-} catch (Exception $e) {
-    echo json_encode(['error' => $e->getMessage()]);
-}
+//header('Content-Type: application/json');
+//
+//
+//
+//try {
+//    $result = ParamsController::fetchParams2("۱۴۰۳/۰۳/۱۲ ۱۳:۰۱:۳۷", "۱۴۰۳/۰۳/۲۸ ۱۳:۰۱:۳۷");
+//
+//    if (empty($result['params'])) {
+//        echo json_encode(['params' => [], 'hasSpeedRecords' => $result['hasSpeedRecords']]);
+//    } else {
+//        echo json_encode($result);
+//    }
+//} catch (Exception $e) {
+//    echo json_encode(['error' => $e->getMessage()]);
+//}
